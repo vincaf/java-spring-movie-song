47dc62e4f9460737b3428ed0211564d25beb36e1
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-12-02 16:47:00 +0100

    Create Movie.java class
    in the pojo package, add getters and setters for id and title

---
+++ BestMovieSong/src/main/java/org/generation/italy/demo/pojo/Movie.java
@@ -0,0 +1,35 @@
+package org.generation.italy.demo.pojo;
+
+public class Movie {
+
+	private int id;
+	private String title;
+	
+	public Movie(int id, String title) {
+		
+		setId(id);
+		setTitle(title);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		
+		return getId() + " - " + getTitle();
+	}
+}
